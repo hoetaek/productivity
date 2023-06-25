aef8f38e2e28d56e567392d309decf9e797854c1
hoetaek <hoetaek@student.snue.ac.kr>
2023-06-25 23:19:16 +0900

    feat: task check workflow 10 to 5 days

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ for task in tasks:
     created_at = datetime.strptime(task["created_at"], "%Y-%m-%dT%H:%M:%S.%fZ")
     days_difference = (datetime.now() - created_at).days
 
-    if days_difference > 0 and days_difference % 10 == 0:
+    if days_difference > 0 and days_difference % 5 == 0:
         print(task["content"])
         print(days_difference)
         create_task_comments(task["id"], f"만들어진지 {days_difference}일 지났습니다.")
